6653a170566d16d1e059704826c26f684f1b7a03
gbahugun <22356761+gbahugun@users.noreply.github.com>
2024-03-09 12:49:28 -0800

    Routes
    Added some user routes and comment routes

--- Server/server.js
+++ Server/server.js
@@ -6,13 +6,10 @@ const db = require('./database/index.js');
 const fs = require('fs');
 const https = require('https');
 const http = require('http')
-const { Server } = require("socket.io");
 const cors = require('cors');
 const UserModel = require('./schemas/User.js')
 const JournalModel = require('./schemas/Journal.js')
 const CommentModel = require('./schemas/Comment.js')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
 
 // Errors
@@ -67,6 +64,118 @@ app.use((err, req, res, next) => {
 app.get('/', (req, res) => {
     res.send("/");
 });
+
+// Create user
+app.post('/api/user/create', asyncWrapper(async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ email: email })
+
+        if(user == undefined){
+            user = await UserModel.create({
+                email: email
+            })    
+            console.log(user);
+            res.status(200).json(user);
+        } else {
+            res.status(200).json("User found")
+        }
+        
+    } catch (err) {
+        throw new DbError("Cannot create user")
+    }
+}))
+
+// Find user
+app.post('/api/user/find', asyncWrapper(async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ email: email })
+
+        if(user != undefined){
+            res.status(200).json(user)
+        } else {
+            res.status(200).json("User not found")
+        }
+        
+    } catch (err) {
+        throw new DbError("Cannot find user")
+    }
+
+}))
+
+// Post comment
+app.post('/api/comment/create', asyncWrapper(async (req, res) =>  {
+    const { userId, journalId, commentText } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ _id: userId })
+
+        const journal = await JournalModel.findOne({ _id: journalId})
+
+        const comment = await CommentModel.create({
+            author: user.userName,
+            commentText: commentText
+        })
+
+        console.log(comment);
+
+        user.comments.push(comment._id);
+        journal.comments.push(comment._id);
+
+        await user.save();
+        await journal.save();
+
+        res.status(200).json(comment);
+    } catch (err) {
+        throw new DbError("Cannot create comment")
+    }
+
+}));
+
+// Get comments by date
+app.post('/api/comment/getByDate', asyncWrapper(async (req, res) =>  {
+    const { journalId } = req.body;
+
+    try {
+        const journal = await JournalModel.findOne({ _id: journalId})
+
+        const commentIds = journal.comments;
+        const comments = [];
+        for(let i = 0; i < commentIds.length; i++) {
+            const comment = await CommentModel.findOne({_id: commentIds[i]})
+            comments.push[comment]
+        }
+
+        res.status(200).json(comments);
+    } catch (err) {
+        throw new DbError("Cannot get comments")
+    }
+}));
+
+// Get comments by likes
+app.post('/api/comment/getByLikes', asyncWrapper(async (req, res) =>  {
+    const { journalId } = req.body;
+
+    try {
+        const journal = await JournalModel.findOne({ _id: journalId})
+
+        const commentIds = journal.comments;
+        const comments = [];
+        for(let i = 0; i < commentIds.length; i++) {
+            const comment = await CommentModel.findOne({_id: commentIds[i]})
+            comments.push[comment]
+        }
+
+        comments.sort((a, b) => a.likes - b.likes)
+
+        res.status(200).json(comments);
+    } catch (err) {
+        throw new DbError("Cannot get comments")
+    }
+}));
 
 // Catch all other routes
 app.get('*', asyncWrapper(async (req, res) => {
